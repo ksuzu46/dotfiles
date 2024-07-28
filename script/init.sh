0372a9384f98e45312edb7f8c610c55be45757b5
keisuke <keisuke.suzuki46@gmail.com>
2024-07-28 19:12:35 +0900

    Use powershell instead of command prompt when running windows commands from wsl2

--- script/init.sh
+++ script/init.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 bin_path=~/.local/bin
-dotfiles_path=~/work/keisuke/dotfiles
+dotfiles_path=~/work/src/github.com/ksuzu46/dotfiles
 
-powershell_path=/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0
+powershell_path=/mnt/c/Program\ Files/PowerShell/7
 office_path=/mnt/c/Program\ Files/Microsoft\ Office/root/Office16
 win_path=/mnt/c/Windows
-system32_path=/mnt/c/WINDOWS/system32
 
 # Path to windows programs
-ln -s $powershell_path/powershell.exe $bin_path/powershell.exe
-ln -s "$office_path"/excel.exe $bin_path/excel.exe
-ln -s "$office_path"/winword.exe $bin_path/winword.exe
-ln -s "$office_path"/powerpnt.exe $bin_path/powerpnt.exe
-ln -s $win_path/explorer.exe $bin_path/explorer.exe
-ln -s $system32_path/cmd.exe $bin_path/cmd.exe
+ln -nfs "$powershell_path"/pwsh.exe $bin_path/pwsh.exe
+ln -nfs "$office_path"/excel.exe $bin_path/excel.exe
+ln -nfs "$office_path"/winword.exe $bin_path/winword.exe
+ln -nfs "$office_path"/powerpnt.exe $bin_path/powerpnt.exe
+ln -nfs $win_path/explorer.exe $bin_path/explorer.exe
 
 # Other path
-ln -s $dotfiles_path/script/excel $bin_path/excel
-ln -s $dotfiles_path/script/word $bin_path/word
-ln -s $dotfiles_path/script/ppnt $bin_path/ppnt
-ln -s $dotfiles_path/script/we $bin_path/we
+ln -nfs $dotfiles_path/script/excel $bin_path/excel
+ln -nfs $dotfiles_path/script/word $bin_path/word
+ln -nfs $dotfiles_path/script/ppnt $bin_path/ppnt
+ln -nfs $dotfiles_path/script/we $bin_path/we
